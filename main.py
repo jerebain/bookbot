6588337a1c27de0a019794f755a97204cb7c5f55
jerebain <jeremy.bain14@gmail.com>
2025-12-16 20:49:38 -0800

    printed report

--- main.py
+++ main.py
@@ -3,13 +3,23 @@ def get_book_text(path_to_file):
         file_contents = f.read()
     return file_contents
 
-def main():
-    print(get_book_text("books/frankenstein.txt"))
-
 from stats import word_count, character_count, sorted_dictionary
 
 contents = get_book_text("books/frankenstein.txt")
 total_words = word_count(contents)
 dictionary_text = character_count(contents)
 sorted = sorted_dictionary(dictionary_text)
-print (f"============ BOOKBOT ============ Analyzing book found at books/frankenstein.txt... ------------ Word Count ------------ Found {total_words} total words ------------ Character Count ------------ {sorted}")
+
+report_start = (
+    f"============ BOOKBOT ============ \n"
+    "Analyzing book found at books/frankenstein.txt... \n"
+    "----------- Word Count ---------- \n"
+    f"Found {total_words} total words \n"
+    "--------- Character Count -------")
+print(report_start)
+for i in sorted:
+    char = i["char"]
+    num = i["num"]
+    if char.isalpha():
+        print(f"{char}: {num}")
+print("============= END ===============")
